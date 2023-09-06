4e49a9f3f1bae325ff5da6a2d5acb6934b2bcc84
direchan <hdjin92@gmail.com>
2023-09-06 11:02:13 +0900

    newpush

--- SleepWellApp/SignUpPage/SignUpPageViewController.swift
+++ SleepWellApp/SignUpPage/SignUpPageViewController.swift
@@ -8,22 +8,37 @@
 import UIKit
 
 class SignUpPageViewController: UIViewController {
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .default
+    }
+    
+    let logoImageView : UIImageView = {
+        let imageView = UIImageView(image: UIImage(named: "Logo"))
+        imageView.contentMode = .scaleAspectFit
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+
+    
+    
+    
 
     override func viewDidLoad() {
+        
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+    
+        view.backgroundColor = UIColor(named: "backgroundColor")
+        view.addSubview(logoImageView)
+        
+        NSLayoutConstraint.activate([
+                logoImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor), //가로위치
+                logoImageView.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -320), //세로위치
+                logoImageView.widthAnchor.constraint(equalToConstant: 132),  // 원하는 너비로 설정
+                logoImageView.heightAnchor.constraint(equalToConstant: 45)  // 원하는 높이로 설정
+            ])
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
