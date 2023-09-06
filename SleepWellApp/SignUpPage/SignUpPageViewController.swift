5a06d5c049149726d16e47be913c1a4a9536f095
direchan <hdjin92@gmail.com>
2023-09-06 13:50:57 +0900

    textFieldAdded

--- SleepWellApp/SignUpPage/SignUpPageViewController.swift
+++ SleepWellApp/SignUpPage/SignUpPageViewController.swift
@@ -19,12 +19,41 @@ class SignUpPageViewController: UIViewController {
         return imageView
     }()
 
+        let nicknameField: UITextField = {
+            let textField = UITextField()
+            textField.placeholder = "5글자 이내"
+            textField.borderStyle = .roundedRect
+            textField.translatesAutoresizingMaskIntoConstraints = false
+            return textField
+        }()
+        
+        let idField: UITextField = {
+            let textField = UITextField()
+            textField.placeholder = "영문, 숫자 조합하여 6~12자"
+            textField.borderStyle = .roundedRect
+            textField.translatesAutoresizingMaskIntoConstraints = false
+            return textField
+        }()
+        
+        let passwordField: UITextField = {
+            let textField = UITextField()
+            textField.placeholder = "영문, 숫자 조합하여 6~12자"
+            textField.borderStyle = .roundedRect
+            textField.translatesAutoresizingMaskIntoConstraints = false
+            return textField
+        }()
+
+
     
     
     
 
     override func viewDidLoad() {
         
+        view.addSubview(nicknameField)
+        view.addSubview(idField)
+        view.addSubview(passwordField)
+
         super.viewDidLoad()
 
     
@@ -32,6 +61,20 @@ class SignUpPageViewController: UIViewController {
         view.addSubview(logoImageView)
         
         NSLayoutConstraint.activate([
+            
+            nicknameField.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            nicknameField.topAnchor.constraint(equalTo: logoImageView.bottomAnchor, constant: 40),
+            nicknameField.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.8),
+
+            idField.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            idField.topAnchor.constraint(equalTo: nicknameField.bottomAnchor, constant: 20),
+            idField.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.8),
+
+            passwordField.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            passwordField.topAnchor.constraint(equalTo: idField.bottomAnchor, constant: 20),
+            passwordField.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.8),
+                
+            
                 logoImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor), //가로위치
                 logoImageView.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -320), //세로위치
                 logoImageView.widthAnchor.constraint(equalToConstant: 132),  // 원하는 너비로 설정
@@ -39,6 +82,7 @@ class SignUpPageViewController: UIViewController {
             ])
     }
     
+
 
 
 }
